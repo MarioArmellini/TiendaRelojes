c942d4ae8d6c1fff4460472939433c9dcf5f0071
Mario Armellini <mario.armellini@opendeusto.es>
2024-10-28 21:04:41 +0100

    Actualizado models.py

--- TiendaRelojesMain/tiendaRelojesAPP/models.py
+++ TiendaRelojesMain/tiendaRelojesAPP/models.py
@@ -2,11 +2,29 @@ from django.db import models
 
 # Create your models here.
 class Filtro(models.Model):
-    precio = models.CharField(max_length=30, null=True)
-    mecanismo = models.CharField(max_length=30, null=True)
-    tipoCorrea = models.CharField(max_length=30, null=True)
+    RANGO_PRECIOS = [
+        ('0-100', '0 a 100€'),
+        ('100-500', '100 a 500€'),
+        ('500-1000', '500 a 1000€')
+        ('1000-5000', '1000 a 5000€')
+        ('10000+', 'Más de 10000€€')
+    ]
+    TIPOS_VISUALIZACION = [
+        ('Digital', 'Digital'),
+        ('Analogico', 'Analógico')
+    ]
+    TIPOS_CORREA = [
+        ('Acero', 'Acero'),
+        ('Cuero', 'Cuero'),
+        ('Tela', 'Tela')
+    ]
+
+    precio = models.CharField(max_length=30, choices=RANGO_PRECIOS, null=True)
+    tipo_visualizacion = models.CharField(max_length=30, choices=TIPOS_VISUALIZACION, null=True)
+    tipo_correa = models.CharField(max_length=30, choices=TIPOS_CORREA, null=True)
+
     def __str__(self):
-        return self.nombreTipo
+        return f"Filtro: {self.precio}, {self.tipo_visualizacion}, {self.tipo_correa}"
 
 class Reloj(models.Model):
     nombre = models.CharField(max_length=30)
