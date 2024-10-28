be18fa6348b4aa096dc15a5d2abc4773c4ee48a5
Mario Armellini <mario.armellini@opendeusto.es>
2024-10-28 20:08:31 +0100

    Creacion models.py

--- TiendaRelojesMain/tiendaRelojesAPP/models.py
+++ TiendaRelojesMain/tiendaRelojesAPP/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Filtro(models.Model):
+    precio = models.CharField(max_length=30, null=True)
+    mecanismo = models.CharField(max_length=30, null=True)
+    tipoCorrea = models.CharField(max_length=30, null=True)
+    def __str__(self):
+        return self.nombreTipo
+
+class Reloj(models.Model):
+    nombre = models.CharField(max_length=30)
+    marca = models.CharField(max_length=30)
+    precio = models.FloatField()
+    Filtro = models.ManyToManyField(Filtro)
+    def __str__(self):
+        return self.marca + ' - ' + self.nombre
+    
+class Compra(models.Model):
+    precioCompra = models.FloatField()
+    Reloj = models.ManyToManyField(Reloj)
+    def __str__(self):
+        return str(self.id) + ': ' + self.Reloj + ' ' + str(self.precioCompra) + '€'
