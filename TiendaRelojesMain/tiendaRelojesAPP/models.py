98f615ca239134b1c0c983ed230235a660fd6f8d
Mario Armellini <mario.armellini@opendeusto.es>
2024-10-29 19:47:46 +0100

    Actualizado models.py, admin.py, creado superUsuario(admin,admin)

--- TiendaRelojesMain/tiendaRelojesAPP/models.py
+++ TiendaRelojesMain/tiendaRelojesAPP/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
-# Create your models here.
 class Filtro(models.Model):
     RANGO_PRECIOS = [
         ('0-100', '0 a 100€'),
         ('100-500', '100 a 500€'),
-        ('500-1000', '500 a 1000€')
-        ('1000-5000', '1000 a 5000€')
-        ('10000+', 'Más de 10000€€')
+        ('500-1000', '500 a 1000€'),
+        ('1000-5000', '1000 a 5000€'),
+        ('10000+', 'Más de 10000€')
     ]
     TIPOS_VISUALIZACION = [
         ('Digital', 'Digital'),
@@ -19,23 +18,26 @@ class Filtro(models.Model):
         ('Tela', 'Tela')
     ]
 
-    precio = models.CharField(max_length=30, choices=RANGO_PRECIOS, null=True)
-    tipo_visualizacion = models.CharField(max_length=30, choices=TIPOS_VISUALIZACION, null=True)
-    tipo_correa = models.CharField(max_length=30, choices=TIPOS_CORREA, null=True)
+    precio = models.CharField(max_length=30, choices=RANGO_PRECIOS, null=True, blank=True)
+    tipo_visualizacion = models.CharField(max_length=30, choices=TIPOS_VISUALIZACION, null=True, blank=True)
+    tipo_correa = models.CharField(max_length=30, choices=TIPOS_CORREA, null=True, blank=True)
 
     def __str__(self):
         return f"Filtro: {self.precio}, {self.tipo_visualizacion}, {self.tipo_correa}"
+
 
 class Reloj(models.Model):
     nombre = models.CharField(max_length=30)
     marca = models.CharField(max_length=30)
     precio = models.FloatField()
-    Filtro = models.ManyToManyField(Filtro)
+    filtro = models.ForeignKey(Filtro, on_delete=models.CASCADE, related_name='relojes', null=True, blank=True)
     def __str__(self):
-        return self.marca + ' - ' + self.nombre
-    
+        return f"{self.marca} - {self.nombre}"
+
+
 class Compra(models.Model):
     precioCompra = models.FloatField()
-    Reloj = models.ManyToManyField(Reloj)
+    reloj = models.ForeignKey(Reloj, on_delete=models.CASCADE, related_name='compras', null=True, blank=True)
+
     def __str__(self):
-        return str(self.id) + ': ' + self.Reloj + ' ' + str(self.precioCompra) + '€'
+        return f"{self.id}: {self.reloj} - {self.precioCompra}€"
